cf2f3cb3fa1cffcc05aae042a791f2d6bf2bfcb9
Ben Macbook Pro <Ben.Maer@gmail.com>
2012-10-08 14:57:19 -0400

    added controller

---
+++ ResplendentUtilities/Controller/HTTPClientJSONAPIController.h
@@ -0,0 +1,37 @@
+//
+//  ECHTTPClientAPIController.h
+//  Everycam
+//
+//  Created by Benjamin Maer on 10/3/12.
+//  Copyright (c) 2012 Resplendent G.P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFHTTPClient.h"
+
+#define kNSErrorMake(_text,_code) [NSError errorWithDomain:_text code:_code userInfo:nil]
+
+@interface HTTPClientJSONAPIController : NSObject
+
+@property (nonatomic, strong) AFHTTPClient* network;
+
+-(id)initWithBaseUrl:(NSString*)baseUrl;
+
+//Delete
+-(void)deleteNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+//Post
+-(void)postNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+//Post multi part with data
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey fileName:(NSString*)fileName mimeType:(NSString*)mimeType noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey noSuccessError:(NSError*)noSuccessError constructingBodyBlock:(void (^)(id <AFMultipartFormData>formData))constructingBodyBlock completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+//Get
+-(void)getNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(id responseJSONParsedObject))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+//Put
+-(void)putNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+@end
---
+++ ResplendentUtilities/Controller/HTTPClientJSONAPIController.m
@@ -0,0 +1,166 @@
+//
+//  ECHTTPClientAPIController.m
+//  Everycam
+//
+//  Created by Benjamin Maer on 10/3/12.
+//  Copyright (c) 2012 Resplendent G.P. All rights reserved.
+//
+
+#import "HTTPClientJSONAPIController.h"
+#import "JSONKit.h"
+#import "AFHTTPRequestOperation.h"
+
+#define kHTTPClientJSONAPIControllerPostMultipartDataNoDataError kNSErrorMake(@"postMultipartDataNetworkRequestWithUrl:params:data:dataParamKey:noSuccessError:completionBlock:failBlock: must have a non-nil data param.",420)
+
+//[NSError errorWithDomain:@"postMultipartDataNetworkRequestWithUrl:params:data:dataParamKey:noSuccessError:completionBlock:failBlock: must have a non-nil data param." code:420 userInfo:nil]
+
+
+@interface HTTPClientJSONAPIController ()
+
+-(void)postSuccessLogicWithReponseObject:(id)responseObject noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+
+-(BOOL)responseDictionaryHasValidSuccessValue:(NSDictionary*)responseDict;
+
+@end
+
+
+
+@implementation HTTPClientJSONAPIController
+
+@synthesize network = _network;
+
+-(id)initWithBaseUrl:(NSString*)baseUrl
+{
+    if (self = [super init])
+    {
+        [self setNetwork:[[AFHTTPClient alloc]initWithBaseURL:[NSURL URLWithString:baseUrl]]];
+    }
+    
+    return self;
+}
+
+#pragma mark - Private methods
+-(void)postSuccessLogicWithReponseObject:(id)responseObject noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
+{
+    NSString* response = [[NSString alloc]initWithData:(NSData*)responseObject encoding:NSUTF8StringEncoding];
+    id responseJSONParsedObject = [response objectFromJSONString];
+    if (noSuccessError)
+    {
+        if (![responseJSONParsedObject isKindOfClass:[NSDictionary class]] || [self responseDictionaryHasValidSuccessValue:responseJSONParsedObject])
+        {
+            if (completionBlock)
+                completionBlock(responseJSONParsedObject);
+        }
+        else
+        {
+            if (failBlock)
+                failBlock(nil,noSuccessError);
+        }
+    }
+    else
+    {
+        if (completionBlock)
+            completionBlock(responseJSONParsedObject);
+    }
+}
+
+-(BOOL)responseDictionaryHasValidSuccessValue:(NSDictionary*)responseDict
+{
+    NSNumber* successValue = [responseDict objectForKey:@"success"];
+    return successValue && [successValue isKindOfClass:[NSNumber class]] && successValue.boolValue;
+}
+
+#pragma mark - Public methods
+#pragma mark Get network requests
+-(void)getNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError *)noSuccessError completionBlock:(void (^)(id))completionBlock failBlock:(void (^)(AFHTTPRequestOperation *operation, NSError *))failBlock
+{
+    [_network getPath:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failBlock)
+            failBlock(operation,error);
+    }];
+}
+
+#pragma mark Post network requests
+-(void)postNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError *)noSuccessError completionBlock:(void (^)(NSDictionary *))completionBlock failBlock:(void (^)(AFHTTPRequestOperation *operation, NSError *))failBlock
+{
+    [_network postPath:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failBlock)
+            failBlock(operation,error);
+    }];
+}
+
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey fileName:(NSString*)fileName mimeType:(NSString*)mimeType noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock;
+{
+    [self postMultipartDataNetworkRequestWithUrl:url params:params data:data dataParamKey:dataParamKey noSuccessError:noSuccessError constructingBodyBlock:^(id<AFMultipartFormData> formData) {
+        [formData appendPartWithFileData:data name:dataParamKey fileName:fileName mimeType:mimeType];
+    } completionBlock:completionBlock progressBlock:progressBlock failBlock:failBlock];
+}
+
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey noSuccessError:(NSError*)noSuccessError constructingBodyBlock:(void (^)(id <AFMultipartFormData>formData))constructingBodyBlock completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
+{
+    if (params && [params objectForKey:dataParamKey] != data)
+    {
+        NSDate* testDate = [NSDate date];
+        NSLog(@"time test started at %@",testDate);
+        NSMutableDictionary* mDict = [NSMutableDictionary dictionaryWithDictionary:params];
+        [mDict setObject:data forKey:dataParamKey];
+        params = [NSDictionary dictionaryWithDictionary:mDict];
+        NSLog(@"time test finished at %f",[[NSDate date] timeIntervalSinceDate:testDate]);
+    }
+
+    NSMutableURLRequest* uploadRequest = uploadRequest = [_network multipartFormRequestWithMethod:@"POST" path:url parameters:params constructingBodyWithBlock:constructingBodyBlock];
+    
+    [uploadRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    AFHTTPRequestOperation* op = [[AFHTTPRequestOperation alloc]initWithRequest:uploadRequest];
+    [op setUploadProgressBlock:^(NSInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+        if (progressBlock)
+        {
+            progressBlock((float)totalBytesWritten / (float)totalBytesExpectedToWrite);
+        }
+    }];
+    
+    [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failBlock)
+            failBlock(operation,error);
+    }];
+    
+    [op start];
+}
+
+-(void)postMultipartDataNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params data:(NSData*)data dataParamKey:(NSString*)dataParamKey noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock progressBlock:(void(^)(float progress))progressBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
+{
+    [self postMultipartDataNetworkRequestWithUrl:url params:params data:data dataParamKey:dataParamKey noSuccessError:noSuccessError constructingBodyBlock:^(id<AFMultipartFormData> formData) {
+        [formData appendPartWithFormData:data name:dataParamKey];
+    } completionBlock:completionBlock progressBlock:progressBlock failBlock:failBlock];
+}
+
+#pragma mark - Put request methods
+-(void)putNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
+{
+    [_network putPath:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failBlock)
+            failBlock(operation,error);
+    }];
+}
+
+#pragma mark - Delete methods
+-(void)deleteNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
+{
+    [_network deletePath:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failBlock)
+            failBlock(operation,error);
+    }];
+}
+
+@end
