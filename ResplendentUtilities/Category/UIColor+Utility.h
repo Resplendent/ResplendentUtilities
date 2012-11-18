1739935338cb0ba8cefb8ef20f524dc99a3dad7a
benmaer <ben.maer@resplendent.co>
2012-11-18 01:02:32 -0500

    changed color creator name

--- ResplendentUtilities/Category/UIColor+Utility.h
+++ ResplendentUtilities/Category/UIColor+Utility.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#define ColorRGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 
 @interface UIColor (Utility)
 
