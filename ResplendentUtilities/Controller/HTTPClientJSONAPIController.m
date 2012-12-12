0333c37c3b1a26677b1be01757b80ad6ba4774bd
Ben Maer <ben@resplendent.co>
2012-12-11 21:53:11 -0500

    fixed put request params

--- ResplendentUtilities/Controller/HTTPClientJSONAPIController.m
+++ ResplendentUtilities/Controller/HTTPClientJSONAPIController.m
@@ -146,7 +146,7 @@
 #pragma mark - Put request methods
 -(void)putNetworkRequestWithUrl:(NSString*)url params:(NSDictionary*)params noSuccessError:(NSError*)noSuccessError completionBlock:(void(^)(NSDictionary* responseDict))completionBlock failBlock:(void(^)(AFHTTPRequestOperation *operation, NSError* error))failBlock
 {
-    [_network putPath:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [_network putPath:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
         [self postSuccessLogicWithReponseObject:responseObject noSuccessError:noSuccessError completionBlock:completionBlock failBlock:failBlock];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         if (failBlock)
