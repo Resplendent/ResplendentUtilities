dfaa1b6f7b0b43dd043f6816229e419f15efecd2
Ben Maer <ben@resplendent.co>
2012-12-27 22:50:55 -0500

    removed ; from rudlog

--- ResplendentUtilities/Misc/RUConstants.h
+++ ResplendentUtilities/Misc/RUConstants.h
@@ -12,7 +12,7 @@
 #define RU_MUST_OVERRIDE [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s must override %@ in a class %@",__PRETTY_FUNCTION__,NSStringFromSelector(_cmd),NSStringFromClass(self.class)] userInfo:nil];
 
 #define RULog(...) NSLog(__VA_ARGS__)
-#define RUDLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#define RUDLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define METHOD_IMPLEMENTATION_NEEDED NSLog(@"@IMPLEMENT Need to implement method %@ in class %@",NSStringFromSelector(_cmd),NSStringFromClass([self class]))
 //lcl_log(RKLogComponent, lcl_vDebug, @"" __VA_ARGS__)
