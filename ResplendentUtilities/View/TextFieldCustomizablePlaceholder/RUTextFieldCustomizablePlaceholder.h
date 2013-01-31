8c2b29b80c2b1a0358b7abcd5e82c34f6d139deb
Ben Maer <ben@resplendent.co>
2013-01-31 01:05:00 -0500

    added text field with customizable placeholder

---
+++ ResplendentUtilities/View/TextFieldCustomizablePlaceholder/RUTextFieldCustomizablePlaceholder.h
@@ -0,0 +1,16 @@
+//
+//  RUTextFieldCustomizablePlaceholder.h
+//  Albumatic
+//
+//  Created by Benjamin Maer on 1/31/13.
+//  Copyright (c) 2013 Resplendent G.P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUTextFieldCustomizablePlaceholder : UITextField
+
+@property (nonatomic, strong) UIColor* placeholderColor;
+@property (nonatomic, strong) UIFont* placeholderFont;
+
+@end
---
+++ ResplendentUtilities/View/TextFieldCustomizablePlaceholder/RUTextFieldCustomizablePlaceholder.m
@@ -0,0 +1,19 @@
+//
+//  RUTextFieldCustomizablePlaceholder.m
+//  Albumatic
+//
+//  Created by Benjamin Maer on 1/31/13.
+//  Copyright (c) 2013 Resplendent G.P. All rights reserved.
+//
+
+#import "RUTextFieldCustomizablePlaceholder.h"
+
+@implementation RUTextFieldCustomizablePlaceholder
+
+-(void)drawPlaceholderInRect:(CGRect)rect
+{
+    [_placeholderColor setFill];
+    [[self placeholder] drawInRect:rect withFont:_placeholderFont];
+}
+
+@end
