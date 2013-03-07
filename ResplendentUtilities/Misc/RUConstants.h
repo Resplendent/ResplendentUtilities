88649bd56c7ba809e9850c79bcecff64c38f6f60
Ben Maer <ben@resplendent.co>
2013-03-06 17:01:29 -0800

    updated some ruconstant rudlog stuff

--- ResplendentUtilities/Misc/RUConstants.h
+++ ResplendentUtilities/Misc/RUConstants.h
@@ -12,10 +12,12 @@
 #define RU_OVERRIDE_LOG NSLog(@"<%@:%d> You must override %@ in a subclass", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, NSStringFromSelector(_cmd));
 #define RU_MUST_OVERRIDE [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s must override %@ in a class %@",__PRETTY_FUNCTION__,NSStringFromSelector(_cmd),NSStringFromClass(self.class)] userInfo:nil];
 
+#define kRUDLogDeclaration(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+
 #if kRUDebugSilenceRUDLog
 #define RUDLog(fmt, ...)
 #else
-#define RUDLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define RUDLog(fmt, ...) kRUDLogDeclaration(fmt, ##__VA_ARGS__)
 #endif
 
 #define RU_METHOD_IMPLEMENTATION_NEEDED RUDLog(@"@IMPLEMENT Need to implement method")
