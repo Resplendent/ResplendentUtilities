60106ba3cf866adfc8902ba5abf8ce12cbc897c9
Ben Maer <ben@resplendent.co>
2013-03-07 22:49:18 -0800

    added method for synthesizing static variables and getters

---
+++ ResplendentUtilities/Misc/RUStaticVariableSynthesization.h
@@ -0,0 +1,26 @@
+//
+//  RUStaticVariableSynthesization.h
+//  Albumatic
+//
+//  Created by Benjamin Maer on 3/7/13.
+//  Copyright (c) 2013 Resplendent G.P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define RUStaticVariableSynthesization(varType,varName) \
+static varType * varName##StaticVariable;
+
+#define RUStaticVariableSynthesizationWithGetter(varType,varName,allocStatment) \
+RUStaticVariableSynthesization(varType,varName); \
+ \
++(varType *)varName \
+{ \
+    if (!varName##StaticVariable) \
+        varName##StaticVariable = allocStatment; \
+    return varName##StaticVariable; \
+}
+
+
+#define RUStaticVariableSynthesizationWithGetterImage(varName,imageNameString) \
+RUStaticVariableSynthesizationWithGetter(UIImage,varName,[UIImage imageNamed:imageNameString]) \
