3c514c932e5cb3622b208b385ea2c2ca78c4f213
Ben Maer <ben@resplendent.co>
2013-04-08 03:01:40 -0700

    added rutableview

---
+++ ResplendentUtilities/View/TableView/RUTableView.h
@@ -0,0 +1,15 @@
+//
+//  RUTableView.h
+//  Pineapple
+//
+//  Created by Benjamin Maer on 4/8/13.
+//  Copyright (c) 2013 Pineapple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUTableView : UITableView
+
+@property (assign) BOOL allowTouchesOnVisibleCells;
+
+@end
