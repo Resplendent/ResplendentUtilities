8dce9b4068f359a45afba67abb12960b99282b44
Ben Maer <ben@resplendent.co>
2013-04-08 18:48:50 -0700

    updated time ago string for rounding to 0 seconds if date is from the future

--- ResplendentUtilities/Category/NSDate+Utility.m
+++ ResplendentUtilities/Category/NSDate+Utility.m
@@ -35,40 +35,6 @@ kRUNSDateCalendarUnit NSDateTimeAgoMinCalendarUnitWithDateCompontents(NSDateComp
 
 @implementation NSDate (Utility)
 
-//-(NSString*)daysOrHoursOrMinutesOrSecondsString
-//{
-//    return [NSDate daysOrHoursOrMinutesOrSecondsStringWithTimeIntervalNow:self.timeIntervalSinceNow];
-//}
-//
-//-(NSTimeInterval)daysOrHoursOrMinutesOrSecondsTypeTimeInterval
-//{
-//    NSTimeInterval timeSince = abs(self.timeIntervalSinceNow);
-//    if (timeSince < 60.0f)                          //Seconds
-//        return 1;
-//    else if (timeSince < 60.0f * 60.0f)             //Minutes
-//        return 60.0f;
-//    else if (timeSince < 60.0f * 60.0f * 24.0f)     //Hours
-//        return 60.0f * 60.0f;
-//    else                                            //Days
-//        return 60.0f * 60.0f * 24.0f;
-//}
-//
-//+(NSString*)daysOrHoursOrMinutesOrSecondsStringWithTimeIntervalNow:(NSTimeInterval)timeInterval
-//{
-//    double timeSince = abs(timeInterval);
-//    NSString* timeLabel = nil;
-//    if (timeSince < 60.0f)                          //Seconds
-//        timeLabel = [NSString stringWithFormat:@"%.fs",floor(timeSince)];
-//    else if (timeSince < 60.0f * 60.0f)             //Minutes
-//        timeLabel = [NSString stringWithFormat:@"%.fm",floor(timeSince / 60.0f)];
-//    else if (timeSince < 60.0f * 60.0f * 24.0f)     //Hours
-//        timeLabel = [NSString stringWithFormat:@"%.fh",floor(timeSince / 60.0f / 60.0f)];
-//    else                                            //Days
-//        timeLabel = [NSString stringWithFormat:@"%.fd",floor(timeSince / 60.0f / 60.0f / 24.0f)];
-//    
-//    return timeLabel;
-//}
-
 -(NSString*)timeAgoString
 {
     NSDateComponents *comps = timoAgoDateComponentsFromDate(self);
@@ -172,7 +138,7 @@ NSInteger NSDateTimeAgoStringAmountWithDateCompontents(NSDateComponents* comps)
             return comps.minute;
             break;
         case kRUNSDateCalendarUnitSecond:
-            return comps.second;
+            return MAX(comps.second, 0);
             break;
 
         default:
@@ -213,7 +179,7 @@ kRUNSDateCalendarUnit NSDateTimeAgoMinCalendarUnitWithDateCompontents(NSDateComp
         return kRUNSDateCalendarUnitSecond;
     }
 
-    return NSNotFound;
+    return kRUNSDateCalendarUnitSecond;
 }
 
 @end
