d842a2dc0581ef3ffde6506488141e7705f7924b
BenMaer <ben@resplendent.co>
2013-10-08 19:15:43 -0400

    updates to RUTextFieldCustomizablePlaceholder

--- ResplendentUtilities/View/TextFieldCustomizablePlaceholder/RUTextFieldCustomizablePlaceholder.m
+++ ResplendentUtilities/View/TextFieldCustomizablePlaceholder/RUTextFieldCustomizablePlaceholder.m
@@ -7,18 +7,68 @@
 //
 
 #import "RUTextFieldCustomizablePlaceholder.h"
+#import "UIView+Utility.h"
 
 @implementation RUTextFieldCustomizablePlaceholder
 
 -(void)drawPlaceholderInRect:(CGRect)rect
 {
-    [_placeholderColor setFill];
-    [[self placeholder] drawInRect:rect withFont:_placeholderFont];
+    if (self.placeholderColor)
+    {
+        [self.placeholderColor setFill];
+    }
+
+    if (self.placeholderFont && self.placeholder.length)
+    {
+        CGFloat fontHeight = self.placeholderFont.pointSize;
+        CGFloat yCoord = CGRectGetVerticallyAlignedYCoordForHeightOnHeight(fontHeight, CGRectGetHeight(rect));
+        CGRect placeholderRect = (CGRect){0, yCoord, CGRectGetWidth(rect), fontHeight};
+        [self.placeholder drawInRect:placeholderRect withFont:self.placeholderFont];
+    }
 }
 
--(CGRect)textRectForBounds:(CGRect)bounds
+-(void)layoutSubviews
 {
-    return CGRectInset(bounds, _placeholderLeftPadding, 0);
+    [super layoutSubviews];
+    for (UIView* subview in self.subviews)
+    {
+        CGRect rect = subview.frame;
+        CGFloat placeholderLeftPadding = self.placeholderLeftPadding;
+        rect.origin.x += placeholderLeftPadding;
+        rect.size.width -= placeholderLeftPadding;
+        [subview setFrame:rect];
+    }
+}
+
+#pragma mark - Setters
+-(void)setPlaceholderFont:(UIFont *)placeholderFont
+{
+    if (self.placeholderFont == placeholderFont)
+        return;
+    
+    _placeholderFont = placeholderFont;
+    
+    [self setNeedsDisplay];
+}
+
+-(void)setPlaceholderColor:(UIColor *)placeholderColor
+{
+    if (self.placeholderColor == placeholderColor)
+        return;
+    
+    _placeholderColor = placeholderColor;
+    
+    [self setNeedsDisplay];
+}
+
+-(void)setPlaceholderLeftPadding:(CGFloat)placeholderLeftPadding
+{
+    if (self.placeholderLeftPadding == placeholderLeftPadding)
+        return;
+
+    _placeholderLeftPadding = placeholderLeftPadding;
+
+    [self setNeedsDisplay];
 }
 
 @end
