5fb403c9f31ecab58a84b30431124cffc554a014
BenMaer <ben@resplendent.co>
2013-12-23 17:15:12 -0500

    removed facebook controller

--- ResplendentUtilities/Code/ResplendentUtilities/Misc/RUConstants.h
+++ ResplendentUtilities/Code/ResplendentUtilities/Misc/RUConstants.h
@@ -7,20 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RUDebug.h"
 
 #define RU_OVERRIDE_LOG NSLog(@"<%@:%d> You must override %@ in a subclass", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, NSStringFromSelector(_cmd));
 #define RU_MUST_OVERRIDE [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s must override %@ in a class %@",__PRETTY_FUNCTION__,NSStringFromSelector(_cmd),NSStringFromClass(self.class)] userInfo:nil];
-
-#define kRUDLogDeclaration(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
-
-#if kRUDebugSilenceRUDLog
-#define RUDLog(fmt, ...)
-#else
-#define RUDLog(fmt, ...) kRUDLogDeclaration(fmt, ##__VA_ARGS__)
-#endif
-
-#define RU_METHOD_IMPLEMENTATION_NEEDED RUDLog(@"@IMPLEMENT Need to implement method")
 
 #define RU_METHOD_OVERLOADED_IMPLEMENTATION_NEEDED_EXCEPTION \
 @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:RUStringWithFormat(@"%@ must overload",NSStringFromClass([self class])) userInfo:nil];
