bdcfaac200bd7eee2364bb247ddb3a6bc536ceb5
BenMaer <ben@resplendent.co>
2014-03-04 11:47:11 -0500

    removed old line

--- ResplendentUtilities/Code/ResplendentUtilities/Misc/RUClassOrNilUtil.h
+++ ResplendentUtilities/Code/ResplendentUtilities/Misc/RUClassOrNilUtil.h
@@ -12,7 +12,6 @@
 id __RUClassOrNilUtilFunction(id val, Class valClass);
 
 #define kRUClassOrNil(val,valClass) ((valClass*)__RUClassOrNilUtilFunction(val,[valClass class]))
-//#define kRUClassOrNil(val,valClass) (val && [val isKindOfClass:[valClass class]] ? (valClass*)val : nil)
 #define kRUNumberOrNil(num) kRUClassOrNil(num,NSNumber)
 #define kRUStringOrNil(str) kRUClassOrNil(str,NSString)
 #define kRUDictionaryOrNil(dict) kRUClassOrNil(dict,NSDictionary)
