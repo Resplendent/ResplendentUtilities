48ac9477471aeaa7d66269df64ca22190a803f18
BenMaer <ben@resplendent.co>
2014-04-20 12:59:23 -0400

    Updates to synthesize user defaults

--- ResplendentUtilities/Code/ResplendentUtilities/Misc/RUSynthesizeUserDefaultMethods.h
+++ ResplendentUtilities/Code/ResplendentUtilities/Misc/RUSynthesizeUserDefaultMethods.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+
+
+
+
+//General
+#define __kRUSynthesizeUserDefaultMethods_AssociatedKey_Name(varName) __RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey
+#define __kRUSynthesizeUserDefaultMethods_AssociatedKey_String(varName) @"__RUSynthesizeUserDefaultMethods_" #varName @"_AssociatedKey"
+
+#define __kRUSynthesizeUserDefaultMethods_Synthesize_AssociatedKey(varName) \
+static NSString* __kRUSynthesizeUserDefaultMethods_AssociatedKey_Name(varName) = __kRUSynthesizeUserDefaultMethods_AssociatedKey_String(varName); \
+
 #define kRUSynthesizeSetUserDefaultsMethodCompletionImplementation(varName,key,completion) \
 NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults]; \
 if (varName && !kRUClassOrNil(varName,NSNull)) [userDefaults setObject:varName forKey:key]; \
@@ -61,9 +72,18 @@ kRUSynthesizeSetUserDefaultsMethodCompletionImplementation(@(varName),key,comple
 kRUSynthesizeSetUserDefaultsMethodCompletionImplementation(varName,key,completion) \
 }
 
-//Static, Implementation Getter
 #define RUSynthesizeStaticSetUserDefaultsMethod(varName,varType,key) \
 RUSynthesizeStaticSetUserDefaultsMethodCompletion(varName,varType,key,nil)
+
+//Static, Implementation Getter
+#define RUSynthesizeStaticGetUserDefaultsMethod_Primitive_SynthesizeKey(varName,varType,NSNumberGetter) \
++(varType)varName \
+{ \
+NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults]; \
+NSString* key = __kRUSynthesizeUserDefaultMethods_AssociatedKey_Name(varName); \
+NSNumber* varNumber = [userDefaults objectForKey:key]; \
+return varNumber.NSNumberGetter; \
+}
 
 #define RUSynthesizeStaticGetUserDefaultsMethod(varName,varType,key) \
 +(varType *)varName \
@@ -73,11 +93,18 @@ return [userDefaults objectForKey:key]; \
 }
 
 //Static, Implementation Setter and Getter
+#define RUSynthesizeStaticSetGetUserDefaultsMethod_Primitive_SynthesizeKey(VarName,varName,varType,NSNumberGetter) \
+__kRUSynthesizeUserDefaultMethods_Synthesize_AssociatedKey(varName) \
+RUSynthesizeStaticSetUserDefaultsMethodCompletion_Primitive(VarName,varType,__RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey,nil) \
+RUSynthesizeStaticGetUserDefaultsMethod_Primitive_SynthesizeKey(varName,varType,NSNumberGetter)
+
 #define RUSynthesizeStaticSetGetUserDefaultsMethod(VarName,varName,varType,key) \
 RUSynthesizeStaticSetUserDefaultsMethod(VarName,varType,key) \
 RUSynthesizeStaticGetUserDefaultsMethod(varName,varType,key)
 
 #define RUSynthesizeStaticSetGetUserDefaultsMethod_SynthesizeKey(VarName,varName,varType) \
-static NSString* __RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey = @"__RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey"; \
+__kRUSynthesizeUserDefaultMethods_Synthesize_AssociatedKey(varName) \
 RUSynthesizeStaticSetUserDefaultsMethod(VarName,varType,__RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey) \
 RUSynthesizeStaticGetUserDefaultsMethod(varName,varType,__RUSynthesizeUserDefaultMethods_##varName##_AssociatedKey)
+
+
