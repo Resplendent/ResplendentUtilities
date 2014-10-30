c849e58ffcbbcfb3ca044f14fb6c3a18af49d655
BenMaer <ben@resplendent.co>
2014-10-30 11:41:21 -0400

    RUModalView: Added -(BOOL)shouldDismissForTapSelfWithTouch:(UITouch*)touch.

--- ResplendentUtilities/Code/ResplendentUtilities/View/Modal/RUModalView.h
+++ ResplendentUtilities/Code/ResplendentUtilities/View/Modal/RUModalView.h
@@ -48,6 +48,7 @@ typedef NS_ENUM(NSInteger, RUModalView_TransitionAnimation_Type) {
 
 @property (nonatomic, readonly) UITapGestureRecognizer* tapGestureRecognizer;
 -(void)didTapSelf:(UITapGestureRecognizer*)tap; //Shouldn't be called directly, meant for subclassing.
+-(BOOL)shouldDismissForTapSelfWithTouch:(UITouch*)touch;
 
 -(void)showInView:(UIView*)presenterView completion:(void(^)())completion;
 -(void)dismiss:(BOOL)animate completion:(void(^)())completion;
