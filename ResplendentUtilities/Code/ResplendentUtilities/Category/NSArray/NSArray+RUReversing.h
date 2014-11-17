ee7c322b6fa43183e222641ab8777c7676234249
BenMaer <ben@resplendent.co>
2014-11-16 20:02:32 -0500

    Added NSArray+RUShuffling, moved all NSArray categories to their own folder.

---
+++ ResplendentUtilities/Code/ResplendentUtilities/Category/NSArray/NSArray+RUReversing.h
@@ -0,0 +1,19 @@
+//
+//  NSArray+RUReversing.h
+//  VibeWithIt
+//
+//  Created by Benjamin Maer on 11/16/14.
+//  Copyright (c) 2014 VibeWithIt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+@interface NSArray (RUReversing)
+
+-(NSMutableArray*)ru_reversedArray;
+
+@end
