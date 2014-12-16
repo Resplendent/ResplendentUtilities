bb5bf2470638e58b7526d15b41ddd87463560025
BenMaer <ben@resplendent.co>
2014-12-16 17:49:18 -0500

    Added UIViewController+RUDismissOrPop.

---
+++ ResplendentUtilities/Code/ResplendentUtilities/Category/UIViewController+RUDismissOrPop.h
@@ -0,0 +1,19 @@
+//
+//  UIViewController+RUDismissOrPop.h
+//  Shimmur
+//
+//  Created by Benjamin Maer on 12/16/14.
+//  Copyright (c) 2014 ShimmurInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+
+@interface UIViewController (RUDismissOrPop)
+
+-(void)ru_dismissOrPop;
+
+@end
