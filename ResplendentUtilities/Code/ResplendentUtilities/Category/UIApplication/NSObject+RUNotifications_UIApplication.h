bc4f3679ef90f53e0728e4fff33362e22fc70ad9
BenMaer <ben@resplendent.co>
2015-01-13 23:34:21 -0800

    - Added NSObject+RUNotifications_UIApplication.  - Updated UINavigationController+RUColoredStatusBarView to adjust for changing status bar.  - Added spacing to RUDeallocHook.

---
+++ ResplendentUtilities/Code/ResplendentUtilities/Category/UIApplication/NSObject+RUNotifications_UIApplication.h
@@ -0,0 +1,22 @@
+//
+//  UIApplication+RUNotifications.h
+//  Shimmur
+//
+//  Created by Benjamin Maer on 1/13/15.
+//  Copyright (c) 2015 ShimmurInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RUNotifications.h"
+
+
+
+
+
+@interface NSObject (RUNotifications_UIApplication)
+
+kRUNotifications_Synthesize_NotificationReadonlySetWithSelectorClearProperty(r,R,egisteredForNotifications_RU_UIApplicationWillChangeStatusBarFrameNotification);
+kRUNotifications_Synthesize_NotificationReadonlySetWithSelectorClearProperty(r,R,egisteredForNotifications_RU_UIApplicationDidChangeStatusBarFrameNotification);
+
+@end
--- ResplendentUtilities/Code/ResplendentUtilities/Category/UINavigationController/UINavigationController+RUColoredStatusBarView.m
+++ ResplendentUtilities/Code/ResplendentUtilities/Category/UINavigationController/UINavigationController+RUColoredStatusBarView.m
@@ -9,12 +9,55 @@
 #import "UINavigationController+RUColoredStatusBarView.h"
 #import "RUSynthesizeAssociatedObjects.h"
 #import "RUConditionalReturn.h"
+#import "NSObject+RUNotifications_UIApplication.h"
+#import "RUDeallocHook.h"
 
 
 
 
 
 NSString* const kUINavigationController_RUColoredStatusBarView_StatusBarBackgroundView_TapAssociatedKey = @"kUINavigationController_RUColoredStatusBarView_StatusBarBackgroundView_TapAssociatedKey";
+NSString* const kUINavigationController_RUColoredStatusBarView_DeallocHookKey = @"kUINavigationController_RUColoredStatusBarView_DeallocHookKey";
+
+static void* kUINavigationController_RUColoredStatusBarView__KVOContext = &kUINavigationController_RUColoredStatusBarView__KVOContext;
+
+
+
+
+
+@interface UINavigationController (_RUColoredStatusBarView)
+
+@property (nonatomic, strong) RUDeallocHook* ru_deallocHook;
+
+-(void)ru_updateStatusBarBackgroundView;
+
+-(void)ru_notificationDidFire_UIApplicationWillChangeStatusBarFrameNotification:(NSNotification*)notification;
+
+@end
+
+
+
+
+
+@implementation UINavigationController (_RUColoredStatusBarView)
+
+-(void)ru_updateStatusBarBackgroundView
+{
+	[self.ru_statusBarBackgroundView setFrame:(CGRect){
+		.size.width		= CGRectGetWidth(self.view.bounds),
+		.size.height	= CGRectGetMinY(self.navigationBar.frame),
+	}];
+}
+
+-(void)ru_notificationDidFire_UIApplicationWillChangeStatusBarFrameNotification:(NSNotification*)notification
+{
+	[self ru_updateStatusBarBackgroundView];
+}
+
+#pragma mark - Synthesize Associated Objects
+RU_Synthesize_AssociatedObject_GetterSetter_Implementation(ru, Ru, _deallocHook, RUDeallocHook*, &kUINavigationController_RUColoredStatusBarView_DeallocHookKey, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+@end
 
 
 
@@ -37,15 +80,23 @@ NSString* const kUINavigationController_RUColoredStatusBarView_StatusBarBackgrou
 		if (self.ru_statusBarBackgroundView == nil)
 		{
 			[self setRu_statusBarBackgroundView:[UIView new]];
-			[self.ru_statusBarBackgroundView setFrame:(CGRect){
-				.size.width		= CGRectGetWidth(self.view.bounds),
-				.size.height	= CGRectGetHeight([UIApplication sharedApplication].statusBarFrame),
-			}];
+			[self ru_updateStatusBarBackgroundView];
 			
 			[self.ru_statusBarBackgroundView setAutoresizingMask:(UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin)];
 			[self.view setAutoresizesSubviews:YES];
 			
 			[self.view addSubview:self.ru_statusBarBackgroundView];
+
+			[self setRegisteredForNotifications_RU_UIApplicationWillChangeStatusBarFrameNotificationOnWithNotificationSelector:@selector(ru_notificationDidFire_UIApplicationWillChangeStatusBarFrameNotification:)];
+			static NSString* const kvoKeyPath_navigationBar_frame = @"center";
+			[self.navigationBar addObserver:self forKeyPath:kvoKeyPath_navigationBar_frame options:0 context:&kUINavigationController_RUColoredStatusBarView__KVOContext];
+
+			[self setRu_deallocHook:[RUDeallocHook deallocHookWithBlock:^{
+
+				[self clearRegisteredForNotifications_RU_UIApplicationWillChangeStatusBarFrameNotification];
+				[self.navigationBar removeObserver:self forKeyPath:kvoKeyPath_navigationBar_frame context:&kUINavigationController_RUColoredStatusBarView__KVOContext];
+
+			}]];
 		}
 		
 		[self.ru_statusBarBackgroundView setBackgroundColor:ru_statusBarBackgroundColor];
@@ -62,5 +113,25 @@ NSString* const kUINavigationController_RUColoredStatusBarView_StatusBarBackgrou
 
 #pragma mark - Synthesize Associated Objects
 RU_Synthesize_AssociatedObject_GetterSetter_Implementation(ru, Ru, _statusBarBackgroundView, UIView*, &kUINavigationController_RUColoredStatusBarView_StatusBarBackgroundView_TapAssociatedKey, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+#pragma mark - KVO
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	if (context == kUINavigationController_RUColoredStatusBarView__KVOContext)
+	{
+		if (object == self.navigationBar)
+		{
+			[self ru_updateStatusBarBackgroundView];
+		}
+		else
+		{
+			NSAssert(false, @"unhandled");
+		}
+	}
+	else
+	{
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+	}
+}
 
 @end
