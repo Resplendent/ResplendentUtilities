f094e6e54aad84f7f9be6ca0af93c71546fdc2eb
Richard Reitzfeld <richie.reitzfeld@gmail.com>
2015-07-07 16:43:02 -0400

    Added Blocks+RUMacros

---
+++ Pod/Classes/ResplendentUtilities/Code/ResplendentUtilities/Macros/Blocks+RUMacros.h
@@ -0,0 +1,16 @@
+//
+//  Blocks+RUMacros.h
+//  Pods
+//
+//  Created by Richard Reitzfeld on 7/7/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+#define kRUCallBlockIfNotNil(block) \
+if (block) {block();}
