7e4214ac085b373a3cdd5b4f9e57d294b1045637
Benjamin Maer <ben@drinkaroo.com>
2017-01-30 12:38:18 -0500

    Updated example pod project

--- Example/Pods/Target Support Files/ResplendentUtilities/ResplendentUtilities-umbrella.h
+++ Example/Pods/Target Support Files/ResplendentUtilities/ResplendentUtilities-umbrella.h
@@ -1,4 +1,14 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
 
 #import "CALayer+RUMask.h"
 #import "CAShapeLayer+RUMessageBox.h"
